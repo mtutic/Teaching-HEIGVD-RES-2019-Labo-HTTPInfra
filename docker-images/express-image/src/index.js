3ec790f654e8000c17609c957d0b3013c910d684
Mateo Tutic <mateo@tutic.ch>
2019-06-08 19:05:52 +0200

    Step2 - comments

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,14 +1,17 @@
 // Include of express
 const express = require('express');
+
 const app = express();
 
 // We use npm package to generate random jokes
 const oneLinerJoke = require('one-liner-joke');
 
+// Handle get requests by sending jokes in a JSON payload
 app.get('/', (req, res) => {
   res.send(oneLinerJoke.getRandomJoke());
 });
 
+// Start the server listening
 app.listen(3000, () => {
   console.log('Accepting HTTP request on port 3000.');
 });
